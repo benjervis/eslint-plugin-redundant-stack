84bb245f3e1e5abe0ca53da53d0598a5f8561c7e
Ben Jervis <bjervis@seek.com.au>
2023-10-26 09:02:07 +1100

    Add some descriptive comments

--- src/check-stack-children.ts
+++ src/check-stack-children.ts
@@ -21,26 +21,31 @@ export const rules: Record<"no-redundant-stack", RuleModule> = {
             return;
           }
 
+          // A self closing Stack won't have any children, and is doing nothing
           if (node.openingElement.selfClosing) {
             return context.report({ node, messageId: "empty" });
           }
 
+          // Filter out the JSXText nodes
           const childElements = node.children.filter(
             (child) =>
               child.type === "JSXElement" ||
               child.type === "JSXExpressionContainer"
           );
 
+          // If there's more than one child, the Stack is doing something
           if (childElements.length > 1) {
             return;
           }
 
+          // A non-self-closed but still empty Stack is redundant
           if (childElements.length === 0) {
             return context.report({ node, messageId: "empty" });
           }
 
           const [child] = childElements;
 
+          // A single child that's an expression container might be a .map or something
           if (child.type !== "JSXExpressionContainer") {
             const childName = child.openingElement.name.name;
             return context.report({
