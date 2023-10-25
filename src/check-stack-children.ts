deab30eefda690993d7ca3768f9a525c27b7bd52
Ben Jervis <bjervis@seek.com.au>
2023-10-25 16:34:37 +1100

    Force JSXElement to be typed

--- src/check-stack-children.ts
+++ src/check-stack-children.ts
@@ -1,35 +1,82 @@
-import 'eslint-plugin-react/lib/types';
-import { Rule } from 'eslint';
+import { Rule } from "eslint";
+import { BaseNode } from "estree";
 
-export const rules: Record<'no-redundant-stack', Rule.RuleModule > = {
-  'no-redundant-stack': {
+/**
+ * This is all required because the JSXElement is included in eslint by default
+ * (via acorn-jsx) but is not in the typescript definitions.
+ */
+
+type JSXElementChildNode = JSXElementNode | JSXExpressionContainerNode;
+
+interface JSXExpressionContainerNode {
+  type: "JSXExpressionContainer";
+  expression: JSXElementNode;
+}
+
+interface JSXOpeningElementNode {
+  type: "JSXOpeningElement";
+  name: JSXIdentifierNode;
+  selfClosing: boolean;
+}
+
+interface JSXIdentifierNode {
+  type: "JSXIdentifier";
+  name: string;
+}
+interface JSXElementNode extends BaseNode {
+  id: string;
+  type: "JSXElement";
+  openingElement: JSXOpeningElementNode;
+  children: JSXElementChildNode[];
+}
+
+declare module "eslint" {
+  namespace Rule {
+    interface NodeListener {
+      JSXElement(node: JSXElementNode): void;
+    }
+
+    interface RuleModule {
+      create(context: RuleContext): NodeListener;
+    }
+  }
+}
+
+declare module "estree" {
+  interface NodeMap {
+    JSXElement: JSXElementNode;
+  }
+}
+
+export const rules: Record<"no-redundant-stack", Rule.RuleModule> = {
+  "no-redundant-stack": {
     meta: {
       docs: {
         description:
-          'A <Stack> element should contain more than one child element.',
+          "A <Stack> element should contain more than one child element.",
       },
       schema: [],
       messages: {
-        oneChild: 'Stack contains only a single <{{ childName }}> element.',
-        empty: 'Stack is empty.',
+        oneChild: "Stack contains only a single <{{ childName }}> element.",
+        empty: "Stack is empty.",
       },
     },
     create: (context) => {
-      const listener =  {
-        JSXElement: (node) => {
+      const listener = {
+        JSXElement: (node: JSXElementNode) => {
           const elementName = node.openingElement.name.name;
-          if (elementName !== 'Stack') {
+          if (elementName !== "Stack") {
             return;
           }
 
           if (node.openingElement.selfClosing) {
-            return context.report({ node, messageId: 'empty' });
+            return context.report({ node, messageId: "empty" });
           }
 
           const childElements = node.children.filter(
             (child) =>
-              child.type === 'JSXElement' ||
-              child.type === 'JSXExpressionContainer',
+              child.type === "JSXElement" ||
+              child.type === "JSXExpressionContainer"
           );
 
           if (childElements.length > 1) {
@@ -37,22 +84,22 @@ export const rules: Record<'no-redundant-stack', Rule.RuleModule > = {
           }
 
           if (childElements.length === 0) {
-            return context.report({ node, messageId: 'empty' });
+            return context.report({ node, messageId: "empty" });
           }
 
           const [child] = childElements;
 
-          if (child.type !== 'JSXExpressionContainer') {
+          if (child.type !== "JSXExpressionContainer") {
             const childName = child.openingElement.name.name;
             return context.report({
               node,
-              messageId: 'oneChild',
+              messageId: "oneChild",
               data: { childName },
             });
           }
         },
       } satisfies Rule.NodeListener;
-      return listener
+      return listener;
     },
   },
 };
